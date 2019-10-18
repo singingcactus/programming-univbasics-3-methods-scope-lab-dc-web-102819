87e2affc33bc3c58066f0c321f3975c310f66af6
Matt Bechtel <bechma02@gmail.com>
2019-10-18 21:30:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,12 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  sword_pickup = 'It\'s Dangerous To Go Alone! Take This.'
+  puts sword_pickup
+end
+
+def any_phrase(phrase)
+  puts phrase
+end
